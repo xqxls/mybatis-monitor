d6fa2e67c7c2a99c5ea05aaa8862a5b396f6af70
xqxls <1078774751@qq.com>
2024-02-06 16:36:34 +0800

    1.添加单元测试

--- mybatis-monitor/src/test/java/com/xqxls/ApiTest.java
+++ mybatis-monitor/src/test/java/com/xqxls/ApiTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @Description:
